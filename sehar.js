4a4131fc53fc6846070c48f996c5aaee87987d31
sehrishraheel <159688133+sehrishraheel@users.noreply.github.com>
2024-03-05 19:32:30 +0500

    Add files via upload

---
+++ sehar.js
@@ -0,0 +1,9 @@
+// camelCase
+var fullName = ("raheel, adeel, shafiq, ashraf");
+console.log(fullName);
+// snake_case
+var full_name = ("raheel, adeel, shafiq, ashraf");
+console.log(full_name);
+// PascalCase
+var FullName = ("raheel, adeel, sshafiq, ashraf");
+console.log(FullName);
---
+++ sehar.ts
@@ -0,0 +1,14 @@
+// camelCase
+
+let fullName: string = ("raheel, adeel, shafiq, ashraf")
+console.log(fullName)
+
+// snake_case
+
+let full_name: string =("raheel, adeel, shafiq, ashraf")
+console.log(full_name)
+
+// PascalCase
+
+let FullName: string = ("raheel, adeel, sshafiq, ashraf")
+console.log(FullName)
